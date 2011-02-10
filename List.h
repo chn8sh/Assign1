de85371cc3a14f808b842520b36d7d0b81495e75
brooks <brooks@Beast.Virginia.EDU>
2011-02-09 23:59:31 -0500

    added ignored files BAck to original init_list. Starting over :'(

--- List.h
+++ List.h
@@ -12,7 +12,6 @@
 #include <stdio.h>
 #include <string.h>
 
-
 /**
  * @class List
  * This C style class (a struct) implements a two doubly-linked list with nodes to hold data
@@ -33,8 +32,7 @@
  *		struct node *next - pointer to next node in the list
  *		void *datum		  - pointer to value in the Node (can be anything)
  */
-typedef struct node
-{
+typedef struct node {
 	struct node *prev, *next;
 	void *datum;
 } Node;
@@ -50,8 +48,7 @@ typedef struct node
  *			for comparing data values
  *		void (*datum_delete)(void *) function pointer to a delete function for deleting nodes or the list
  */
-typedef struct
-{
+typedef struct {
 	Node *head, *tail;
 	unsigned length;
 	int (*compare)(const void *this, const void *other);
@@ -70,17 +67,14 @@ typedef struct
  * @pre  each pointer belongs to a valid place in memory
  * @post data fields are initialized
  */
-void list_init(List *ltemp,
-			   int (*compare)(const void *this, const void *other),
-			   void (*datum_delete)(void *datum))
-{
+void list_init(List *l, int(*compare)(const void *this, const void *other),
+		void(*datum_delete)(void *datum)) {
 
-  ltemp = (List *)malloc( sizeof(List) );
-  List l = *ltemp;
-  l.head = l.tail = NULL;
-  l.length = 0;
-  l.compare = compare;
-  l.datum_delete = datum_delete;
+	l = (List *) malloc(sizeof(List));
+	l->head = l->tail = NULL;
+	l->length = 0;
+	l->compare = compare;
+	l->datum_delete = datum_delete;
 }
 
 //Part 3: Patrick
@@ -92,9 +86,7 @@ void list_init(List *ltemp,
  * @pre  description
  * @post description
  */
-void list_visit_items(List *l,
-					  void (*visitor)(void *v))
-{
+void list_visit_items(List *l, void(*visitor)(void *v)) {
 
 }
 
@@ -107,9 +99,44 @@ void list_visit_items(List *l,
  * @pre  description
  * @post description
  */
-void list_insert_tail(List *l, void *v)
-{
+void list_insert_tail(List *l, void *v) {
+	// initialize variables
+	Node new, *probe;
+	new.datum = v;
 
+	if (l != NULL) {
+		// insert new
+		if (l->length == 0) // first item in the list
+
+		{
+			l->head = l->tail = &new;
+			new.prev = new.next = NULL;
+		} else // length > 1; 3 cases
+		{
+			// locate position to insert
+			for (probe = l->head; probe != NULL; probe = probe->next)
+				if (l->compare(new.datum, probe->datum) <= 0)
+					break;
+			// determine case
+			if (probe == l->head) // case 1: new belongs before first Node
+			{
+				new.prev = NULL;
+				new.next = probe;
+				probe->prev = l->head = &new;
+			} else if (probe == NULL) // case 2: ran off list
+			{
+				new.prev = l->tail;
+				new.next = NULL;
+				l->tail->next = l->tail = &new;
+			} else // case 3: somewhere in the middle of the list
+			{
+				new.prev = probe->prev;
+				new.next = probe;
+				probe->prev->next = probe->prev = &new;
+			}
+		}
+		l->length++; // increase size
+	}
 }
 
 //Part 5: Chris
@@ -122,48 +149,42 @@ void list_insert_tail(List *l, void *v)
  * @pre  list points to a initialized list and v point to a vaild datum
  * @post v is inserted into l
  */
-void list_insert_sorted(List *l, void *v)
-{
+void list_insert_sorted(List *l, void *v) {
 	// initialize variables
 	Node new, *probe;
 	new.datum = v;
 
-	if (l != NULL) {
-		// insert new
-		if(l->length == 0) // first item in the list
-
+	// insert new
+	if (l->length == 0) // first item in the list
+	{
+		l->head = l->tail = &new;
+		new.prev = new.next = NULL;
+	} else // length > 1; 3 cases
+	{
+		// locate position to insert
+		for (probe = l->head; probe != NULL; probe = probe->next)
+			if (l->compare(new.datum, probe->datum) <= 0)
+				break;
+		// determine case
+		if (probe == l->head) // case 1: new belongs before first Node
 		{
-			l->head = l->tail = &new;
-			new.prev = new.next = NULL;
-		}
-		else // length > 1; 3 cases
+			new.prev = NULL;
+			new.next = probe;
+			probe->prev = l->head = &new;
+		} else if (probe == NULL) // case 2: ran off list
 		{
-			// locate position to insert
-			for(probe = l->head; probe != NULL; probe = probe->next)
-				if( l->compare(new.datum, probe->datum) <= 0 )
-					break;
-			// determine case
-			if(probe == l->head) // case 1: new belongs before first Node
-			{
-				new.prev = NULL;
-				new.next = probe;
-				probe->prev = l->head = &new;
-			}
-			else if(probe == NULL) // case 2: ran off list
-			{
-				new.prev = l->tail;
-				new.next = NULL;
-				l->tail->next = l->tail = &new;
-			}
-			else // case 3: somewhere in the middle of the list
-			{
-				new.prev = probe->prev;
-				new.next = probe;
-				probe->prev->next = probe->prev = &new;
-			}
+			new.prev = l->tail;
+			new.next = NULL;
+			l->tail->next = l->tail = &new;
+		} else // case 3: somewhere in the middle of the list
+		{
+			new.prev = probe->prev;
+			new.next = probe;
+			probe->prev->next = probe->prev = &new;
 		}
-		l->length++; // increase size
 	}
+	l->length++; // increase size
+
 }
 
 //Part 6: Brooks
@@ -175,8 +196,7 @@ void list_insert_sorted(List *l, void *v)
  * @pre  description
  * @post description
  */
-void list_remove_head(List *list)
-{
+void list_remove_head(List *list) {
 
 }
 
